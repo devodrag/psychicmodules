ed38062984a351ce1d13fef6c93c1eec5c82dbdd
DevoDrag <euesswekgpt@gmail.com>
2025-08-26 11:43:55 +0300

    Update makvin.py

--- makvin.py
+++ makvin.py
@@ -30,4 +30,53 @@ class MakvinInsult(loader.Module):
             "сказал", "убегай", "нахуй", "отсюда", "терпила", "ебанная", "ты", "же",
             "попросту", "здесь", "не", "выживешь", "под", "гнетом", "и", "издевками",
             "падаль", "ебучая", "наислабейшая", "чернейшая", "проституточная", "я", "тебе",
-            "тут", "нахуй", "все
+            "тут", "нахуй", "все", "зубы", "отобью", "пидорасище", "кулаками", "за",
+            "твои", "пропиздоны", "ебанная", "тряпка", "я", "тебе", "говорю", "вытирай",
+            "слезы", "и", "показывай", "мне", "противостояние", "ведь", "ты", "ебанная",
+            "тварь", "здесь", "будешь", "ловить", "в", "свои", "округлости", "маслянистые",
+            "модернизированные", "харчевые", "слезы", "нахуй", "ну", "ты", "девочка",
+            "рыдаешь", "не", "вывезешь", "эту", "битву", "тупая", "шалава", "соси",
+            "член", "ебанашка", "у", "тя", "мать", "сдохла", "отец", "пьёт", "сестра",
+            "сосет", "мне", "член", "а", "брат", "берет", "в", "жопу", "ну", "скажи",
+            "еще", "мне", "аргументы", "у", "тя", "их", "нет", "ахаха", "все", "завали",
+            "ебало", "шавка", "тупорылая", "ублюдская", "уебанская", "долбаебская",
+            "пока", "шалава", "ной", "в", "хуй", "мой", "пж"
+        ]
+
+    @loader.command()
+    async def makvin(self, message):
+        """Отправляет список оскорблений с заданной задержкой"""
+        await message.delete()
+        for insult in self.insults:
+            await message.respond(insult)
+            await asyncio.sleep(self.config["delay"])
+
+    @loader.command()
+    async def addword(self, message):
+        """Добавляет слово в список оскорблений: .addword <слово>"""
+        args = utils.get_args_raw(message)
+        if not args:
+            await utils.answer(message, self.strings["no_args"])
+            return
+        
+        word = args.strip()
+        if word not in self.insults:
+            self.insults.append(word)
+            await utils.answer(message, self.strings["word_added"].format(word))
+        else:
+            await utils.answer(message, f"❌ Слово <b>{word}</b> уже есть в списке!")
+
+    @loader.command()
+    async def removeword(self, message):
+        """Удаляет слово из списка оскорблений: .removeword <слово>"""
+        args = utils.get_args_raw(message)
+        if not args:
+            await utils.answer(message, self.strings["no_args"])
+            return
+        
+        word = args.strip()
+        if word in self.insults:
+            self.insults.remove(word)
+            await utils.answer(message, self.strings["word_removed"].format(word))
+        else:
+            await utils.answer(message, self.strings["word_not_found"].format(word))
